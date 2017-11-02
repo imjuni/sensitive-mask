ad184809b985850ac70961e0a39684d83881b5e6
Lee ByungJoon <bjlee@yanolja.com>
2017-11-02 15:27:04 +0900

    export MASK_TYPE, IMasker
    * export MASK_TYPE, IMasker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Masker as ko } from './i18n/ko/Masker';
 import { IMasker } from './IMasker';
 
+export * from './IMasker';
+export * from './MASK_TYPE';
+
 export default function Masker(i18n: string = 'ko', maskChar: string = '*'): IMasker {
   if (i18n === 'ko') {
     return new ko(maskChar);
